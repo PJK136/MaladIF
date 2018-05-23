0caf8db543ede5b458d982645765f1404607145c
yugismop <ismail.jattioui@insa-lyon.fr>
2018-05-23 18:17:10 +0200

    Suite de l'implementation Database

--- main.cpp
+++ main.cpp
@@ -2,9 +2,10 @@
 
 #include "CLI.h"
 #include "CLI.h"
-#include "FileReader.h"
+#include "Database.h"
 
 int main() {
+    /*
     Metadata m = FileReader::readMetadata("metadata3.txt");
 
     afficheMetadonnees(m);
@@ -20,6 +21,17 @@ int main() {
         fi = fr.nextFingerprint();
     }
     std::string vide;
-    std::cin >> vide;
+    std::cin >> vide;*/
+
+    Database database;
+    bool success = database.loadMetadata("metadata1.txt");
+    std::cout<<"avant charg"<<std::endl;
+    database.displayDataBase();
+    std::cout<<success<<std::endl;
+    bool succ = database.loadData("Data.txt");
+    std::cout<<"apres charg"<<std::endl;
+    std::cout<<succ<<std::endl;
+    database.displayDataBase();
     return 0;
 }
+
