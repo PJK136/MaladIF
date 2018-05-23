e79c7c038d7f62aa6ebd5e8cf679e012b81efd58
Valentin <valentin.rigolle@insa-lyon.fr>
2018-05-23 18:24:38 +0200

    Tests TU2 et surcharge de l'opérateur == pour Fingerprint

--- Fingerprint.h
+++ Fingerprint.h
@@ -10,4 +10,21 @@ struct Fingerprint
     std::vector<std::variant<std::monostate,bool,int,double,std::string>> values;
 };
 
+inline bool operator==(Fingerprint const& f1, Fingerprint const& f2)
+{
+    if(f1.values.size() != f2.values.size())
+    {
+        return false;
+    }
+
+    for (int i=0; i<f1.values.size(); i++)
+    {
+        if(f1.values[i] != f2.values[i])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
 #endif // FINGERPRINT_H
---
+++ tests/TU2/TU2.cpp
@@ -0,0 +1,63 @@
+#include "FileReader.h"
+
+#include <iostream>
+#include "gtest/gtest.h"
+
+TEST(TU2,a) {
+    Metadata metadata(FileReader::readMetadata("metadata.txt"));
+    FileReader fr;
+    bool open(fr.open("data2.txt",metadata));
+    ASSERT_TRUE(open);
+
+    std::pair<Fingerprint, std::string> line(fr.nextFingerprint());
+    Fingerprint fp;
+    fp.values.push_back(1);
+    fp.values.push_back(std::string("True"));
+    fp.values.push_back(2.12);
+    fp.values.push_back(13.);
+    fp.values.push_back(3.156);
+    fp.values.push_back(1236.);
+    EXPECT_EQ(line.first,fp);
+    EXPECT_EQ(line.second,"Maladie1");
+
+    line = fr.nextFingerprint();
+    line = fr.nextFingerprint();
+    Fingerprint fp2;
+    fp2.values.push_back(2);
+    fp2.values.push_back(std::string("False"));
+    fp2.values.push_back(1.1);
+    fp2.values.push_back(14.3);
+    fp2.values.push_back(13.2);
+    fp2.values.push_back(2367.);
+
+    EXPECT_EQ(line.first,fp2);
+    EXPECT_EQ(line.second,"");
+}
+
+TEST(TU2,b1) {
+    Metadata metadata(FileReader::readMetadata("metadata123456.txt"));
+    FileReader fr;
+    bool open(fr.open("data2.txt",metadata));
+    ASSERT_FALSE(open);
+}
+
+TEST(TU2,b2) {
+    Metadata metadata(FileReader::readMetadata("metadataVIDE.txt"));
+    FileReader fr;
+    bool open(fr.open("data2.txt",metadata));
+    ASSERT_FALSE(open);
+}
+
+TEST(TU2,b3) {
+    Metadata metadata(FileReader::readMetadata("metadata3.txt"));
+    FileReader fr;
+    bool open(fr.open("data2.txt",metadata));
+    ASSERT_TRUE(open);
+}
+
+TEST(TU2,b4) {
+    Metadata metadata(FileReader::readMetadata("metadata4.txt"));
+    FileReader fr;
+    bool open(fr.open("data2.txt",metadata));
+    ASSERT_FALSE(open);
+}
