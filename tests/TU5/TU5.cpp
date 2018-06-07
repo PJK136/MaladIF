3e81a4d667f9b85bb9301047079e139a9abf5b3d
Alexandra Verrier <averrier@insa-lyon.fr>
2018-06-07 15:32:29 +0200

    TU5 fini (avec gestion des erreurs)

--- tests/TU5/TU5.cpp
+++ tests/TU5/TU5.cpp
@@ -28,9 +28,7 @@ TEST(TU5, a1) {
     d.risk = 0.5;
     EXPECT_EQ(*it2, d);
     it2++;
-    d.disease = "";
-    d.risk = 0.; //à calculer
-    //EXPECT_EQ(*it2, d);
+    EXPECT_EQ((*it2).disease, "");
 }
 
 //analyse d'un fichier d'empreintes contenant plusieurs empreintes
@@ -59,9 +57,7 @@ TEST(TU5, a2) {
     d.risk = 0.5;
     EXPECT_EQ(*it2, d);
     it2++;
-    d.disease = "";
-    d.risk = 0.; //valeur à calculer
-    //EXPECT_EQ(*it2, d);
+    EXPECT_EQ((*it2).disease, "");
 
     fp.values = {2,std::string("False"),1.1,14.3,std::monostate(),2367.};
     it++;
@@ -71,14 +67,6 @@ TEST(TU5, a2) {
     d.disease = "";
     d.risk = 1.;
     EXPECT_EQ(*it3, d);
-    it2++;
-    d.disease = "Maladie1";
-    d.risk = 0.; //à calculer
-    //EXPECT_EQ(*it3, d);
-    it2++;
-    d.disease = "Maladie2";
-    d.risk = 0.; //à calculer
-    //EXPECT_EQ(*it3, d);
 }
 
 //fichier d'emplreinte vide
@@ -101,6 +89,83 @@ TEST(TU5, b2) {
     ASSERT_TRUE(database.loadData("data1.txt"));
     EXPECT_EQ(database.error(), Database::Error::OK);
     std::list<std::pair<Fingerprint,std::vector<Diagnosis>>> diagnosis (database.diagnose("doesntExist.txt"));
-    EXPECT_EQ(database.error(), Database::Error::NOT_FOUND);
+    EXPECT_EQ(database.error(), Database::Error::CANT_OPEN);
     ASSERT_EQ(diagnosis.size(), 0);
+}
+
+//fichier d'emplreinte illisible
+TEST(TU5, b3) {
+    Database database;
+    ASSERT_TRUE(database.loadMetadata("metadata1.txt"));
+    EXPECT_EQ(database.error(), Database::Error::OK);
+    ASSERT_TRUE(database.loadData("data1.txt"));
+    EXPECT_EQ(database.error(), Database::Error::OK);
+    std::list<std::pair<Fingerprint,std::vector<Diagnosis>>> diagnosis (database.diagnose("data_bad1.txt"));
+    EXPECT_EQ(database.error(), Database::Error::INVALID);
+    ASSERT_EQ(diagnosis.size(), 0);
+}
+
+//fichier d'empreintes avec des attributs en plus
+TEST(TU5, b4) {
+    Database database;
+    ASSERT_TRUE(database.loadMetadata("metadata1.txt"));
+    EXPECT_EQ(database.error(), Database::Error::OK);
+    ASSERT_TRUE(database.loadData("data1.txt"));
+    EXPECT_EQ(database.error(), Database::Error::OK);
+    std::list<std::pair<Fingerprint,std::vector<Diagnosis>>> diagnosis (database.diagnose("diagnosis_bad1.txt"));
+    EXPECT_EQ(database.error(), Database::Error::INVALID);
+    ASSERT_EQ(diagnosis.size(), 0);
+}
+
+//fichier d'empreintes avec des attributs en moins dans la déclaration des attributs
+TEST(TU5, b5) {
+    Database database;
+    ASSERT_TRUE(database.loadMetadata("metadata1.txt"));
+    EXPECT_EQ(database.error(), Database::Error::OK);
+    ASSERT_TRUE(database.loadData("data1.txt"));
+    EXPECT_EQ(database.error(), Database::Error::OK);
+    std::list<std::pair<Fingerprint,std::vector<Diagnosis>>> diagnosis (database.diagnose("diagnosis_bad2.txt"));
+    EXPECT_EQ(database.error(), Database::Error::INVALID);
+    ASSERT_EQ(diagnosis.size(), 0);
+}
+
+//fichier d'empreintes avec des attributs en moins sur la première empreinte
+TEST(TU5, c1) {
+    Database database;
+    ASSERT_TRUE(database.loadMetadata("metadata1.txt"));
+    EXPECT_EQ(database.error(), Database::Error::OK);
+    ASSERT_TRUE(database.loadData("data1.txt"));
+    EXPECT_EQ(database.error(), Database::Error::OK);
+    std::list<std::pair<Fingerprint,std::vector<Diagnosis>>> diagnosis (database.diagnose("diagnosis_bad3.txt"));
+    EXPECT_EQ(database.error(), Database::Error::INVALID);
+    ASSERT_EQ(diagnosis.size(), 0);
+}
+
+//fichier d'empreintes avec la 2ème empreinte non valide
+TEST(TU5, c2) {
+    Database database;
+    ASSERT_TRUE(database.loadMetadata("metadata1.txt"));
+    EXPECT_EQ(database.error(), Database::Error::OK);
+    ASSERT_TRUE(database.loadData("data1.txt"));
+    EXPECT_EQ(database.error(), Database::Error::OK);
+    std::list<std::pair<Fingerprint,std::vector<Diagnosis>>> diagnosis (database.diagnose("diagnosis_bad4.txt"));
+    EXPECT_EQ(database.error(), Database::Error::INVALID);
+    ASSERT_EQ(diagnosis.size(), 1);
+
+    Fingerprint fp;
+    fp.values = {1,std::string("True"),2.12,13.,3.156,1236.};
+    std::list<std::pair<Fingerprint,std::vector<Diagnosis>>>::iterator it = diagnosis.begin();
+    EXPECT_EQ((*it).first, fp);
+    EXPECT_EQ(((*it).second).size(), 3);
+    std::vector<Diagnosis>::iterator it2 = (*it).second.begin();
+    Diagnosis d;
+    d.disease = "Maladie2";
+    d.risk = 1.;
+    EXPECT_EQ(*it2, d);
+    it2++;
+    d.disease = "Maladie1";
+    d.risk = 0.5;
+    EXPECT_EQ(*it2, d);
+    it2++;
+    EXPECT_EQ((*it2).disease, "");
 }
